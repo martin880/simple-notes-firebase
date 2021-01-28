09fb3302f605ea2f3b9777067936b888d089d526
Martin Lumban Gaol <martinlumbangaol880@gmail.com>
2021-01-28 13:38:11 +0700

    Setup Redux-Thunk

--- src/containers/pages/Login/index.js
+++ src/containers/pages/Login/index.js
@@ -2,19 +2,33 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Login extends Component {
+    changeUser = () => {
+        this.props.changeUserName()
+    }
     render(){
         return(
             <div>
-                <p>Login Page {this.props.popupProps}</p>
-                <button>Go To Register</button>
+                <p>Login Page {this.props.userName}</p>
+                <button onClick={this.changeUser}>Change User Name</button>
                 <button>Go To Dashboard</button>
             </div>
         )
     }
 }
 
+const actionUserName = () => (dispatch) => {
+        setTimeout(()=> {
+            return  dispatch ({type: 'CHANGE_USER', value: 'Martin Lumbangaol'})
+        }, 2000)
+}
+
 const reduxState = (state) =>({
-    popupProps: state.popup
+    popupProps: state.popup,
+    userName: state.user
 })
 
-export default connect(reduxState, null)(Login);
+const reduxDispatch = (dispatch) => ({
+    changeUserName: () => dispatch(actionUserName())
+})
+
+export default connect(reduxState, reduxDispatch)(Login);
