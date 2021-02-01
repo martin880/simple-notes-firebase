ae3ddf2fc3d9ecc92173cea79b3a52c2d4b80ee2
Martin Lumban Gaol <martinlumbangaol880@gmail.com>
2021-02-01 11:19:24 +0700

    Setup Redux Thunk Untuk Proses Register

--- src/config/redux/action/index.js
+++ src/config/redux/action/index.js
@@ -1,5 +1,24 @@
+import firebase from '../../firebase';
+
 export const actionUserName = () => (dispatch) => {
     setTimeout(()=> {
         return  dispatch ({type: 'CHANGE_USER', value: 'Martin Lumbangaol'})
     }, 2000)
+}
+
+export const registerUserAPI = (data) => (dispatch) => {
+    dispatch({type: 'CHANGE_LOADING', value: true})
+    return (
+        firebase.auth().createUserWithEmailAndPassword(data.email, data.password)
+        .then((res) => {
+          console.log('Success: ', res);
+          dispatch({type: 'CHANGE_LOADING', value: false})
+        })
+        .catch((error) => {
+          var errorCode = error.code;
+          var errorMessage = error.message;
+          console.log(errorCode, errorMessage)
+          dispatch({type: 'CHANGE_LOADING', value: false})
+        })
+    )
 }
--- src/containers/pages/Register/index.js
+++ src/containers/pages/Register/index.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './Register.scss';
-import firebase from '../../../config/firebase';
+import Button from '../../../components/atoms/Button';
+import { connect } from 'react-redux';
+import { registerUserAPI } from '../../../config/redux/action';
 
 class Register extends Component {
     state = {
         email: '',
-        password: ''
+        password: '',
     }
 
     handleChangeText = (e) => {
@@ -17,15 +19,7 @@ class Register extends Component {
     handleRegisterSubmit = () => {  
         const {email, password} = this.state;
         console.log('data before send: ', email, password)
-        firebase.auth().createUserWithEmailAndPassword(email, password)
-        .then((res) => {
-          console.log('Success: ', res);
-        })
-        .catch((error) => {
-          var errorCode = error.code;
-          var errorMessage = error.message;
-          console.log(errorCode, errorMessage)
-        });
+        this.props.registerAPI({email, password}) 
     }
 
     render(){
@@ -35,7 +29,8 @@ class Register extends Component {
                     <p className="auth-title">Register Page</p>
                     <input className="input" id="email" placeholder="Email" type="text" onChange={this.handleChangeText}/>
                     <input className="input" id="password" placeholder="Password" type="password" onChange={this.handleChangeText}/>
-                    <button className="btn" onClick={this.handleRegisterSubmit}>Register</button>
+                    {/* <button className="btn" onClick={this.handleRegisterSubmit}>Register</button> */}
+                    <Button onClick={this.handleRegisterSubmit} title="Register" loading={this.props.isLoading}/>
                 </div>
                 {/* <button>Go To Dashboard</button> */}
             </div>
@@ -43,4 +38,12 @@ class Register extends Component {
     }
 }
 
-export default Register;
+const reduxState = (state) => ({
+    isLoading: state.isLoading
+})
+
+const reduxDispatch = (dispatch) => ({
+    registerAPI: (data) => dispatch(registerUserAPI(data))
+})
+
+export default connect(reduxState, reduxDispatch)(Register);
